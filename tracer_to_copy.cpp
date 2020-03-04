5b82c10078ef08bd92d691d39892a8962fd186bc
Cammie Smith Barnes <cammie@chromium.org>
2020-03-04 13:00:22 -0500

    add error mesage

--- tracer_to_copy.cpp
+++ tracer_to_copy.cpp
@@ -9,6 +9,8 @@
 std::string filepath("../trace/output.txt");
 std::ofstream outfile;
 outfile.open(filepath, std::fstream::app);
+if (!outfile.is_open())
+  LOG(ERROR) << "file not opened: try running with --no-sandbox";
 outfile << "*****" << std::endl;
 outfile << base::debug::StackTrace().ToString();
 outfile.close();
